59c24b45c135d9ce7471eb9ba1869a103440d064
RZL <1120082028@qq.com>
2025-05-13 23:18:10 +0800

    1

--- src/main/electronContext/userContext.ts
+++ src/main/electronContext/userContext.ts
@@ -5,6 +5,7 @@ registerContext('post', '/user', async (opt: any) => {
     let user = await db.users.findOne({
         where: { username: opt.body.username }
     })
-    console.log(user)
-    return user.toJSON();
+    if (user) {
+        return user.toJSON();
+    }
 })
